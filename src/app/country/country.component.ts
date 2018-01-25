f2e6ac752bf8db77e6aa9900de9ae5ac8c07ad16
RiteshPatkar <riteshkptkar@gmail.com>
2018-01-25 07:06:44 -0500

    Changes around add button functionality

--- src/app/country/country.component.ts
+++ src/app/country/country.component.ts
@@ -15,11 +15,12 @@ import { IndexKind } from "typescript";
 
 export class CountryComponent implements OnInit {
   
+  @Input() country: Country;
+  @Input() updatedCountries: Country[];
+  
 //  countryArray: Observable<Country[]>;
    countryArray: Country[];
   countryFormGroup : FormGroup;
-  @Input() country: Country;
-  @Input() updatedCountries: Country[];
   isLoading = false;
   showNewRow = false;
   
@@ -28,6 +29,65 @@ export class CountryComponent implements OnInit {
     private countryService: CountryService) {
     alert('constructor');
     this.createFormGroup();
+  }
+  
+   createFormGroup() {
+    alert('createFormGroup');
+    this.countryFormGroup = this.countryFormBuilder.group({
+      countries : this.countryFormBuilder.array([])
+    });
+  }
+  
+   ngOnChanges() {
+    alert('onChange');
+    this.countryFormGroup.reset({
+    });
+      this.setCountries(this.updatedCountries);
+  }
+  
+  get countries(): FormArray {
+    return this.countryFormGroup.get('countries') as FormArray
+  }
+  
+  //compare again with example
+   setCountries(countries : Country[]) {
+    alert('setCountries');
+    const countriesFormGroups = countries.map(country => this.countryFormBuilder.group(country));
+    const countryFormArray = this.countryFormBuilder.array(countriesFormGroups);
+    this.countryFormGroup.setControl('countries', countryFormArray);
+  }
+  
+    add() {
+    alert('in Add');
+    this.showNewRow = true;
+    this.countries.push(this.countryFormBuilder.group(new Country()));
+  }
+  
+    submit() {
+    alert('submit')
+      this.showNewRow = false;
+    this.updatedCountries = this.prepareForCRUD();
+     alert(JSON.stringify(this.updatedCountries));
+//    this.countryService.updateCountries(this.updatedCountries).subscribe(/* error handling */);
+      let updatedCountries = this.countryService.updateCountries(this.updatedCountries);
+       this.createFormGroup();
+     alert(JSON.stringify(this.updatedCountries));
+    this.setCountries(this.updatedCountries);
+  }
+  
+    prepareForCRUD(): Country[] {
+    const formModel = this.countryFormGroup.value;
+      alert(JSON.stringify(formModel));
+    const countriesDeepCopy: Country[] = formModel.countries.map(
+      (country: Country) => Object.assign({}, country));
+      alert(countriesDeepCopy.length);
+    return countriesDeepCopy;
+  }
+  
+  revert() { 
+    this.createFormGroup();
+    this.getCountriesFromService(); 
+    this.setCountries(this.countryArray);
   }
   
   ngOnInit() { 
@@ -40,52 +100,6 @@ export class CountryComponent implements OnInit {
     alert('getCountriesFromService');
     this.isLoading = true;
     this.countryArray = this.countryService.getCountries();
-  }
-  
-  
-  createFormGroup() {
-    alert('createFormGroup');
-    this.countryFormGroup = this.countryFormBuilder.group({
-      countries : this.countryFormBuilder.array([]),
-      country : this.countryFormBuilder.group({
-        flag: ['', Validators.required],
-        countryDescription: ['', Validators.required],
-        countryCode: ['', Validators.required],
-        postalCodeLength: '',
-      })
-    });
-  }
-  
-//  ngOnChanges() {
-//    this.countryFormGroup.reset({
-//      countryDescriptionFormControl: this.country.countryDescription,
-//      countryCodeFormControl: this.country.countryCode,
-//      postalCodeLengthFormControl: this.country.postalCodeLength,
-//      countryFlagFormControl: this.country.flag
-//    });
-//    this.setCountries(this.countryArray);
-//  }
-  
-    get countries(): FormArray {
-    return this.countryFormGroup.get('countries') as FormArray
-  }
-  
-  setCountries(countries : Country[]) {
-    alert('setCountries');
-    const countriesFormGroups = countries.map(country => this.countryFormBuilder.group(country));
-    const countryFormArray = this.countryFormBuilder.array(countriesFormGroups);
-    this.countryFormGroup.setControl('countries', countryFormArray);
-  }
-  
-  revert() { 
-    this.createFormGroup();
-    this.getCountriesFromService(); 
-    this.setCountries(this.countryArray);
-  }
-  
-  add() {
-    this.showNewRow = true;
-    this.countries.push(this.countryFormBuilder.group(new Country()));
   }
   
   delete(i : number, country : Country) {
@@ -115,24 +129,7 @@ export class CountryComponent implements OnInit {
     }
   }
   
-   submit() {
-    alert('submit')
-      this.showNewRow = false;
-    this.updatedCountries = this.prepareForCRUD();
-//    this.countryService.updateCountries(this.updatedCountries).subscribe(/* error handling */);
-      let updatedCountries = this.countryService.updateCountries(this.updatedCountries);
-       this.createFormGroup();
-    this.setCountries(this.updatedCountries);
-  }
-  
-    prepareForCRUD(): Country[] {
-    const formModel = this.countryFormGroup.value;
-    const countriesDeepCopy: Country[] = formModel.countries.map(
-      (country: Country) => Object.assign({}, country)
-    ).fill(formModel.country);
-      alert(countriesDeepCopy.length);
-    return countriesDeepCopy;
-  }
+
   
 
 }
--- src/app/mock-data/mock-countries.ts
+++ src/app/mock-data/mock-countries.ts
@@ -1,14 +1,14 @@
 import { Country } from '../data/country';
 
 export const COUNTRIES: Country[] = [
-  { flag: 'Y', countryCode: 'US', countryDescription: 'America', currencyISOCode: 'USD', postalCodeLength: 5, postalCodePosition: 'Before' },
-  { flag: 'Y', countryCode: 'AD', countryDescription: 'Andorra', currencyISOCode: 'ESP', postalCodeLength: 5, postalCodePosition: 'Before' },
-  { flag: 'Y', countryCode: 'AE', countryDescription: 'Arab Ameirites', currencyISOCode: 'AED', postalCodeLength: 10, postalCodePosition: 'Before' },
-  { flag: 'Y', countryCode: 'AF', countryDescription: 'Afganistan', currencyISOCode: 'AFN', postalCodeLength: 10, postalCodePosition: 'Before' },
-  { flag: 'Y', countryCode: 'AG', countryDescription: 'Antyigua & Barbuda', currencyISOCode: 'XCD', postalCodeLength: 5, postalCodePosition: 'Before' },
-  { flag: 'Y', countryCode: 'FR', countryDescription: 'France', currencyISOCode: 'EUR', postalCodeLength: 5, postalCodePosition: 'Before' },
-  { flag: 'Y', countryCode: 'AD', countryDescription: 'Andorra', currencyISOCode: 'ESP', postalCodeLength: 5, postalCodePosition: 'Before' },
-  { flag: 'Y', countryCode: 'AE', countryDescription: 'Arab Ameirites', currencyISOCode: 'AED', postalCodeLength: 10, postalCodePosition: 'Before' },
+//  { flag: 'Y', countryCode: 'US', countryDescription: 'America', currencyISOCode: 'USD', postalCodeLength: 5, postalCodePosition: 'Before' },
+//  { flag: 'Y', countryCode: 'AD', countryDescription: 'Andorra', currencyISOCode: 'ESP', postalCodeLength: 5, postalCodePosition: 'Before' },
+//  { flag: 'Y', countryCode: 'AE', countryDescription: 'Arab Ameirites', currencyISOCode: 'AED', postalCodeLength: 10, postalCodePosition: 'Before' },
+//  { flag: 'Y', countryCode: 'AF', countryDescription: 'Afganistan', currencyISOCode: 'AFN', postalCodeLength: 10, postalCodePosition: 'Before' },
+//  { flag: 'Y', countryCode: 'AG', countryDescription: 'Antyigua & Barbuda', currencyISOCode: 'XCD', postalCodeLength: 5, postalCodePosition: 'Before' },
+//  { flag: 'Y', countryCode: 'FR', countryDescription: 'France', currencyISOCode: 'EUR', postalCodeLength: 5, postalCodePosition: 'Before' },
+//  { flag: 'Y', countryCode: 'AD', countryDescription: 'Andorra', currencyISOCode: 'ESP', postalCodeLength: 5, postalCodePosition: 'Before' },
+//  { flag: 'Y', countryCode: 'AE', countryDescription: 'Arab Ameirites', currencyISOCode: 'AED', postalCodeLength: 10, postalCodePosition: 'Before' },
   { flag: 'Y', countryCode: 'AF', countryDescription: 'Afganistan', currencyISOCode: 'AFN', postalCodeLength: 10, postalCodePosition: 'Before' },
   { flag: 'Y', countryCode: 'AG', countryDescription: 'Antyigua & Barbuda', currencyISOCode: 'XCD', postalCodeLength: 5, postalCodePosition: 'Before' }
 ];
